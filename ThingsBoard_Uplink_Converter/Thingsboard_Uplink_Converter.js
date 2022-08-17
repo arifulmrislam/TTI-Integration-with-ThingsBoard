dd0b70be3d85dfc7aacd4f1b02e9df1d463761f3
ARIFUL ISLAM ARIF <ariful.mr.islam@gmail.com>
2022-08-17 10:40:18 +0300

    Update Thingsboard_Uplink_Converter.js

--- ThingsBoard_Uplink_Converter/Thingsboard_Uplink_Converter.js
+++ ThingsBoard_Uplink_Converter/Thingsboard_Uplink_Converter.js
@@ -1,8 +1,3 @@
-// It's decoding the payload from hexadecimal to string
-// It's parsing the string to JSON
-// It's extracting the device name and type from the JSON
-// It's extracting the telemetry data from the JSON
-// It's returning a JSON object with the device name, type and telemetry data
 var data = decodeToJson(payload);
 var deviceName = data.end_device_ids.device_id;
 var deviceType = data.end_device_ids.application_ids.application_id;
@@ -15,25 +10,28 @@ switch (first1) {                       // prepare the right telemetry
   case "e":                             // ext-therm-fridge-adeunis-temp
     telemetry = {temperature: data.uplink_message.decoded_payload.temperature, low_bat: data.uplink_message.decoded_payload.low_bat};
   break;   
-  case "m":                             // modbus-a-adeunis & modbus-b-adeunis
+  case "m":                             // modbus_a_adeunis & modbus-b-adeunis
     switch (data.uplink_message.decoded_payload.payload_type) {
       case 1:                           // payload type 1
-        telemetry = {vr: data.uplink_message.decoded_payload.vr, vs: data.uplink_message.decoded_payload.vs, vt: data.uplink_message.decoded_payload.vt, p_tot: data.uplink_message.decoded_payload.p_tot};
+        telemetry = {vr: data.uplink_message.decoded_payload.vr, vs: data.uplink_message.decoded_payload.vs, vt: data.uplink_message.decoded_payload.vt, p_tot: data.uplink_message.decoded_payload.p_tot, payload_type: data.uplink_message.decoded_payload.payload_type};
       break;
       case 2:                           // payload type 2
-        telemetry = {e_tot: data.uplink_message.decoded_payload.e_tot};
+        telemetry = {e_tot: data.uplink_message.decoded_payload.e_tot, e_depext: data.uplink_message.decoded_payload.e_depext, payload_type: data.uplink_message.decoded_payload.payload_type};
       break;
     }
   break;
-  case "c":                             // contact-a-adeunis-drycontact & contact-b-adeunis-drycontact
+  case "c":                             // contact_a_adeunis_drycontact & contact_b_adeunis_drycontact
     switch (data.uplink_message.decoded_payload.payload_type) {
       case 1:                           // payload 1 count type
-        telemetry = {count: data.uplink_message.decoded_payload.count, low_bat: data.uplink_message.decoded_payload.low_bat}
+        telemetry = {counter: data.uplink_message.decoded_payload.counter, low_bat: data.uplink_message.decoded_payload.low_bat, poweron:data.uplink_message.decoded_payload.poweron}
       break;
       case 2:                           // payload 2 time duration type
         telemetry = {duration: data.uplink_message.decoded_payload.duration, low_bat: data.uplink_message.decoded_payload.low_bat}
       break;
     }
+  break;
+  case "b":                             // busy light
+    telemetry = {red: data.uplink_message.decoded_payload.lastcolor_red, blue: data.uplink_message.decoded_payload.lastcolor_blue, green: data.uplink_message.decoded_payload.lastcolor_green};
   break;
 }
 var result = {
